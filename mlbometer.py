b32abf72e3190cc668fae4efc01ae36b70dbeedf
wawzat <wawzat@gmail.com>
2021-05-31 14:16:26 -0700

    Add F to message if game is final

--- mlbometer.py
+++ mlbometer.py
@@ -227,8 +227,10 @@ def get_games(spoiler, start_date, end_date):
         home_score = game['home_score']
         away_score = game['away_score']
         home_str = f"{home_name} ({home_score})"
-        away_str = f"{away_name} ({away_score})"
-
+        if 'Final' not in status:
+            away_str = f"{away_name} ({away_score})"
+        else:
+            away_str = f"{away_str} F"
         home_team = statsapi.get('team', {'teamId':home_id})
         away_team = statsapi.get('team', {'teamId':away_id})
         home_league = home_team['teams'][0]['league']['id']
